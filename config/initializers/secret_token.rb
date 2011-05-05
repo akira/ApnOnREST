cfdeec112ab0f5238c4b69001429e95dc35ef74a
Alex Kira <alex@getbeezy.com>
2011-05-05 17:43:48 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApn::Application.config.secret_token = 'ddcf70dfb2d69d6b9cb285050a6149e49f32192569ebf59735ebfefcdc234cbfc812dd8752daaffaf1b770b3328985b4d04c7bb1fa3272c7b0a20ed3f6bb05e4'
