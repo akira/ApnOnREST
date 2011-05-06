6631a9d696aa3574d864429db25066a2905c6f99
Alex Kira <alex@getbeezy.com>
2011-05-06 13:51:28 -0400

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  helper_method :logged_in?
+
+  def logged_in?
+    session[:login]
+  end
+
+  private
+  def authenticate
+    login = authenticate_or_request_with_http_basic do |username, password|
+      username == "username" && password == "password"
+    end
+    session[:login] = login
+  end
+
+  def do_logout
+    session[:login] = nil
+  end
 end
